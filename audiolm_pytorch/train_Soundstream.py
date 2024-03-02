3e3e52ab945293983a76e682cc218f1ab15aa36a
Chang Cheng <ccheng.app@gmail.com>
2024-03-02 15:59:42 -0800

    turn on wandb

--- audiolm_pytorch/train_Soundstream.py
+++ audiolm_pytorch/train_Soundstream.py
@@ -18,7 +18,8 @@ trainer = SoundStreamTrainer(
     data_max_length = 320 * 32,
     save_results_every = 100,
     save_model_every = 100,
-    num_train_steps = 1000
+    num_train_steps = 1000, 
+    use_wandb_tracking=True
 ).cuda()
 # NOTE: I changed num_train_steps to 9 (aka 8 + 1) from 10000 to make things go faster for demo purposes
 # adjusting save_*_every variables for the same reason
